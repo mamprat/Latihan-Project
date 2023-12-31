e28de101429161a85c1a28dd24ab0a5781d98ebb
Imam Pratama <imampratamasetiady11@gmail.com>
2023-12-31 08:33:20 +0700

    Revert "file json ori"
    This reverts commit 4aabdf38ffa9639eee15f8a38ec4a2b9cec225e9.

--- arduino_code/arduino_code.ino
+++
@@ -1,8 +0,0 @@
-void setup() {
- Serial.begin(9600); // Starts the serial communication at 9600 baud
-}
-
-void loop() {
- Serial.println("Hello World"); // Sends the string "Hello World" over serial
- delay(1000); // Waits for a second before sending the next string
-}
